436fd653794308afe9e0c4d15b927a8ac39882a1
sonsurim <surim014@naver.com>
2022-03-25 21:06:59 +0900

    🔧 config: Typescript 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const tsConfigPath = path.resolve(__dirname, "./tsconfig.json")
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js',
     clean: true
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    plugins:[
+      new TsconfigPathsPlugin({
+        configFile: tsConfigPath
+      })
+    ]
   },
 	module: {
 		rules: [
@@ -18,6 +28,11 @@ module.exports = {
           filename: 'assets/images/[name][ext]'
         },
       },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
 		]
 	},
 	devtool: "inline-source-map",
