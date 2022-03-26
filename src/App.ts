fff9a0d1027a8bebe60839d4a84113be5d277d6f
sonsurim <surim014@naver.com>
2022-03-26 17:06:14 +0900

    ✨ feat: SearchInput 적용

--- src/App.ts
+++ src/App.ts
@@ -1,11 +1,42 @@
-import { Component } from '@components'
+import { Component, SearchInput } from '@components'
+import type { IAppComponentState, IComponentParams } from '@models'
+import { selectEl } from '@utils'
 
-export default class App extends Component<null> {
+export default class App extends Component<IAppComponentState> {
+  constructor({ node }: IComponentParams<IAppComponentState>) {
+    const initalState = {
+      keyword: ''
+    }
+
+    super({ node, initalState })
+  }
+
   template(): string {
     return `
       <main id="#App">
         <div>App Component</div>
+        <SearchInput></SearchInput>
       </main>
     `
+  }
+
+  attachChildComponent(): void {
+    new SearchInput({
+      node: selectEl(this.node, 'SearchInput'),
+      initalState: {
+        keyword: '',
+        onChange: (keyword): void => {
+          this.setState({
+            keyword
+          })
+
+          if (!keyword) {
+            return
+          }
+
+          console.log('api 호출!', this.state)
+        }
+      }
+    })
   }
 }
