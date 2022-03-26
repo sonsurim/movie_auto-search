e9c246e9637d0de7dbb60df9726fddf9fbab8fde
sonsurim <surim014@naver.com>
2022-03-27 01:35:29 +0900

    💄 ui: SearchResultList 마크업

--- src/components/domain/search/SearchResultList/index.ts
+++ src/components/domain/search/SearchResultList/index.ts
@@ -1,17 +1,19 @@
 import { Component } from '@components'
+import styles from './SearchResult.module.scss'
 import type { ISearchResultList } from './types'
 
 export default class SearchResultList extends Component<ISearchResultList> {
   template(): string {
+    const { listWrapper, selected } = styles
     const { listData, listVisible, currentKeywordId } = this.state
 
     return `
-    <ul class="${listVisible ? '' : 'hide'}">
+    <ul class="${listVisible ? '' : 'hide'} ${listData.length && listWrapper}">
       ${listData
         ?.map(({ id, text }, idx) => {
           return `
             <li data-id="${id}" ${
-            currentKeywordId === idx + 1 ? 'style="color: red;"' : ''
+            currentKeywordId === idx + 1 ? `class="${selected}"` : ''
           }>${text}</li>`
         })
         .join('')}
